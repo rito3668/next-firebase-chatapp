d2e7d40af74b9d1cea8b7540ec181feb9fbeca65
Ritabrata Pal <rito3668@gmail.com>
2024-01-08 15:15:30 +0530

    bugfix

--- components/popup/UsersPopup.jsx
+++ components/popup/UsersPopup.jsx
@@ -62,7 +62,7 @@ const UsersPopup = (props) => {
       <div className='mt-5 flex flex-col gap-2 grow relative overflow-auto scrollbar'>
         <div className='absolute w-full'>
           {users && Object.values(users).map((user)=>(
-            <div className="flex items-center gap-4 rounded-xl hover:bg-c5 py-2 px-4 cursor-pointer" onClick={()=>handleSelect(user)}>
+            <div key={user.id} className="flex items-center gap-4 rounded-xl hover:bg-c5 py-2 px-4 cursor-pointer" onClick={()=>handleSelect(user)}>
               <Avatar size="large" user={user}/>
               <div className='flex flex-col gap-1 grow'>
                 <span className='text-base text-white flex items-center justify-between'>
